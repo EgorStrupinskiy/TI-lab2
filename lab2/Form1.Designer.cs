59aa9bf206319daa86ec29adbd92856de2987adc
EgorStrupinskiy <venik2405@yandex.ru>
2022-04-28 19:20:57 +0300

    all files added

---
+++ lab2/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxRegister = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1378, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.файлToolStripMenuItem.Text = "File";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.открытьToolStripMenuItem.Text = "Open";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 64);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(642, 139);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(642, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Key";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(710, 64);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(308, 139);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(1040, 64);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.ReadOnly = true;
+            this.richTextBox3.Size = new System.Drawing.Size(308, 228);
+            this.richTextBox3.TabIndex = 4;
+            this.richTextBox3.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(705, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(307, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Input";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(1035, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(307, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Output";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(710, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(302, 72);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Cipher";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Form1_Load);
+            // 
+            // textBoxRegister
+            // 
+            this.textBoxRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxRegister.Location = new System.Drawing.Point(12, 270);
+            this.textBoxRegister.Name = "textBoxRegister";
+            this.textBoxRegister.Size = new System.Drawing.Size(642, 26);
+            this.textBoxRegister.TabIndex = 8;
+            this.textBoxRegister.Text = "111111111111111111111111111111111111111";
+            this.textBoxRegister.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(179, 243);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(307, 23);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Register";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Moccasin;
+            this.ClientSize = new System.Drawing.Size(1378, 313);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxRegister);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Струпинский Егор, lab2";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxRegister;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ lab2/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime;
+using System.Collections;
+using System.Text;
+
+namespace lab2
+{
+    public partial class Form1 : Form
+    {
+
+        private BitArray defaultBits;
+        private BitArray key;
+        private string fileName;
+        byte[] bytes;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private byte[] ReadtBytes(String file)
+        {
+            using (FileStream fsSource = new FileStream(file, FileMode.Open, FileAccess.Read))
+            {
+                byte[] bytes = new byte[fsSource.Length];
+                int numBytesToRead = (int)fsSource.Length;
+                int numBytesRead = 0;
+                while (numBytesToRead > 0)
+                {
+                    int n = fsSource.Read(bytes, numBytesRead, numBytesToRead);
+                    if (n == 0)
+                        break;
+
+                    numBytesRead += n;
+                    numBytesToRead -= n;
+                }
+                fsSource.Close();
+                return bytes;
+            }
+        }
+
+        private BitArray GetBitArray(byte[] bytes)
+        {
+            BitArray bits = new BitArray(bytes.Length * 8);
+            int index = 0;
+            foreach (byte b in bytes)
+            {
+                byte[] a = new byte[] { b };
+                BitArray singleByte = new BitArray(a);
+                for (int i = singleByte.Length - 1; i >= 0; i--)
+                {
+                    bits[index++] = singleByte[i];
+                }
+            }
+            return bits;
+        }
+
+        private void PrintBits(BitArray bits, RichTextBox textBox)
+        {
+            int max = bits.Length > 100 ? 100 : bits.Length;
+            for (int i = 0; i < max; i++)
+            {
+                textBox.Text = textBox.Text + Convert.ToByte(bits[i]);
+            }
+        }
+
+        private void GenerateKey(int length)
+        {
+            key = new BitArray(length);
+            while (textBoxRegister.Text.Length < 39)
+                textBoxRegister.Text = textBoxRegister.Text + '0';
+            BitArray register = new BitArray(39, false);
+            for (int i = 0; i < register.Length; i++)
+            {
+                if (textBoxRegister.Text[i].Equals('1'))
+                    register[i] = true;
+            }
+            for (int i = 0; i < length; i++)
+            {
+                key[i] = register[0];
+                bool xor = register[0] ^ register[35];
+                for (int j = 0; j < 38; j++)
+                {
+                    register[j] = register[j + 1];
+                }
+                register[38] = xor;
+            }
+        }
+
+        public BitArray xor(String a, String b)
+        {
+            BitArray bits = new BitArray(a.Length);
+            StringBuilder sb = new StringBuilder();
+            for (int k = 0; k < a.Length; k++)
+                sb.Append(a[k] ^ b[k + (Math.Abs(a.Length - b.Length))]);
+            for (int i = 0; i < a.Length; i++)
+            {
+                bits[i] = sb[i].Equals("1");
+            }
+            return bits;
+        }
+
+        private byte[] BitsToBytes(BitArray bits)
+        {
+            byte[] bytes = new byte[bits.Length / 8];
+            int index = 0;
+            int pow = 7;
+            foreach (bool bit in bits)
+            {
+                bytes[index / 8] += Convert.ToByte(bit ? Math.Pow(2, pow) : 0);
+                //Console.WriteLine(bytes[index / 8]);
+                index++;
+                pow = pow == 0 ? 7 : pow - 1;
+            }
+            return bytes;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            GenerateKey(defaultBits.Length);
+            PrintBits(key, richTextBox1);
+           // defaultBits = xor(defaultBits.ToString(), key.ToString());
+            defaultBits.Xor(key);
+            PrintBits(defaultBits, richTextBox3);
+            bytes = BitsToBytes(defaultBits);
+            using (FileStream fsNew = new FileStream("result.txt", FileMode.Create, FileAccess.Write))
+            {
+                fsNew.Write(bytes, 0, bytes.Length);
+            }
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            richTextBox2.Text = "";
+            richTextBox3.Text = "";
+            openFileDialog1.ShowDialog();
+            fileName = openFileDialog1.FileName;
+            button1.Enabled = true;
+            bytes = ReadtBytes(fileName);
+            defaultBits = GetBitArray(bytes);
+            PrintBits(defaultBits, richTextBox2);
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
